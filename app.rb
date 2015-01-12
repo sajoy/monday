6a2f68b20551f703fb41b95508f70d1d4c3d8746
Tyler Whitsett & Sarah Joy Calpo <student@epicodus.com>
2015-01-12 15:09:25 -0800

    add triangle sinatra app, and an error redirect for both apps.

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require ('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/parcel')
 
-get ('/') do
+get('/') do
+  @title = "Home"
+  erb(:home)
+end
+
+get ('/parcel_form') do
+  @title = "Parcel Cost Calculator"
   erb(:form)
 end
 
@@ -14,5 +20,24 @@ get ('/results') do
   @weight = params.fetch('weight').to_f()
   @parcel = Parcel.new(@length, @width, @depth, @weight)
   @cost = @parcel.cost()
+  @title = "Parcel Cost Calculator"
+  if @length.==(0).|(@width.==(0)).|(@depth.==(0)).|(@weight.==(0))
+    redirect "/parcel_form?@redirect=true"
+  end
   erb(:results)
+end
+
+get ('/triangle_form') do
+  @title = "Triangle Type"
+  erb(:form_triangle)
+end
+
+get ('/results_triangle') do
+  @side1 = params.fetch('side1').to_f()
+  @side2 = params.fetch('side2').to_f()
+  @side3 = params.fetch('side3').to_f()
+  @triangle_a = Triangle.new(@side1, @side2, @side3)
+  @triangle = @triangle_a.type()
+  @title = "Triangle Type"
+  erb(:results_triangle)
 end
